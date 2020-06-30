151cd128b296ea95bd7d145a6fa3f457ff1cb887
Josh Williams <joshwill221@gmail.com>
2020-06-30 22:25:39 +0800

    Updates

---
+++ precache-manifest.45086cc9947884551aa6e10f615e817d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2123a82bd93553e0415d5f5558df0e8",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "e8a5401cefa258d4462f",
+    "url": "/simple-todo-app/static/css/main.cfa66741.chunk.css"
+  },
+  {
+    "revision": "517448722c259d425d02",
+    "url": "/simple-todo-app/static/js/2.bd25b82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.bd25b82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a5401cefa258d4462f",
+    "url": "/simple-todo-app/static/js/main.60ccf778.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
