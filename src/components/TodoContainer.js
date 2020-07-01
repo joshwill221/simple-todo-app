04b1a70c5e446f2b6a382aeb1235c677964ca011
Josh Williams <joshwill221@gmail.com>
2020-07-01 12:27:06 +0800

    Added HTTP requests and React lifecycle methods

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,78 +1,87 @@
-import React from "react"
+import React from "react";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
-import Header from "./Header"
-import TodosList from "./TodosList"
-import InputTodo from "./InputTodo"
+import Header from "./Header";
+import TodosList from "./TodosList";
+import InputTodo from "./InputTodo";
 
 class TodoContainer extends React.Component {
-    state = {
+  state = {
+    todos: [],
+    show: false
+  };
+
+  handleChange = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+      show: !this.state.show
+    });
+  };
+
+  delTodo = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then((response) =>
+      this.setState({
         todos: [
-            {
-                id: uuidv4(),
-                title: "Setup development enivonment",
-                completed: true
-            },
-            {
-                id: uuidv4(),
-                title: "Develop website and add content",
-                completed: false
-            },
-            {
-                id: uuidv4(),
-                title: "Deploy to live server",
-                completed: false
-            }
-        ]
-    };
+          ...this.state.todos.filter((todo) => {
+            return todo.id !== id;
+          }),
+        ],
+      })
+    );
+    this.setState({
+      todos: [
+        ...this.state.todos.filter((todo) => {
+          return todo.id !== id;
+        }),
+      ],
+    });
+  };
 
-    handleChange = (id) => {
+  addTodoItem = (title) => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title: title,
+        completed: false,
+      })
+      .then((response) =>
         this.setState({
-            todos: this.state.todos.map(todo => {
-                if (todo.id === id) {
-                    todo.completed = !todo.completed;
-                }
-                return todo;
-            })
-        });
-    };
+          todos: [...this.state.todos, response.data],
+        })
+      );
+  };
 
-    delTodo = id => {
-        this.setState({
-            todos: [
-                ...this.state.todos.filter(todo => {
-                    return todo.id !== id;
-                })
-            ]
-        });
-    };
+  componentDidMount() {
+    // https://jsonplaceholder.typicode.com/todos?_limit=10
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos", {
+        params: {
+          _limit: 10,
+        },
+      })
+      .then((response) => this.setState({ todos: response.data }));
+  }
 
-    addTodoItem = title => {
-        const newTodo = {
-            id: uuidv4(),
-            title: title,
-            completed: false
-        };
-        this.setState({
-            todos: [...this.state.todos, newTodo]
-        });
-    };
-
-    render() {
-        return (
-            <div className="container">
-                <Header />
-                <InputTodo addTodoProps={this.addTodoItem}/>
-                <TodosList
-                    todos={this.state.todos}
-                    handleChangeProps={this.handleChange}
-                    deleteTodoProps={this.delTodo}
-                />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <Header headerSpan={this.state.show}/>
+        <InputTodo addTodoProps={this.addTodoItem} />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
+      </div>
+    );
+  }
 }
 
-export default TodoContainer
+export default TodoContainer;
 
 // If don't want external div, use <React.fragment> (or use shortcut, <></>) instead of <div>
